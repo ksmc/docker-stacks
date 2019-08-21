0999294fc521ebbf755fda00d632ee113fc12d7a
Ziang Jia <zjia@ksmconsulting.com>
2019-08-21 12:59:53 -0400

    images in development

--- build-all.sh
+++ build-all.sh
@@ -9,7 +9,7 @@ docker build -t mphanaenvacr.azurecr.io/scipy-notebook:latest .
 docker push mphanaenvacr.azurecr.io/scipy-notebook:latest
 cd ..
 
-# drived from scipy-notebook
+# drived from scipy-notebook & r-notebook
 cd datascience-notebook
 docker build -t mphanaenvacr.azurecr.io/datascience-notebook:latest .
 docker push mphanaenvacr.azurecr.io/datascience-notebook:latest
@@ -25,9 +25,17 @@ docker build -t mphanaenvacr.azurecr.io/pyspark-notebook:latest .
 docker push mphanaenvacr.azurecr.io/pyspark-notebook:latest
 cd ..
 
+cd rstudio-notebook
+docker build -t mphanaenvacr.azurecr.io/rstudio-notebook:latest .
+docker push mphanaenvacr.azurecr.io/rstudio-notebook:latest
+cd ..
+
+# drived from pyspark-notebook
 cd all-spark-notebook
 docker build -t mphanaenvacr.azurecr.io/all-spark-notebook:latest .
 docker push mphanaenvacr.azurecr.io/all-spark-notebook:latest
 cd ..
+
+
 
 
