86da069e7b9f5c33f3d162699c30dc356972d4f2
Ziang Jia <zjia@ksmconsulting.com>
2019-10-02 16:33:56 -0400

    flesh out acrname from dockerfiles

--- build-all.sh
+++ build-all.sh
@@ -1,64 +1,65 @@
-# Login sandboxmphanaenvacr 
-az acr login -n sandboxmphanaenvacr
+# Login sandboxmphanaenvacr
+az login 
+az acr login -n mphjphk8sacr
 
 # build jupyter-k8s-hub
 cd jupyter-k8s-hub
-docker build -t sandboxmphanaenvacr.azurecr.io/k8s-hub:latest .
-docker push sandboxmphanaenvacr.azurecr.io/k8s-hub:latest
+docker build -t mphjphk8sacr.azurecr.io/k8s-hub:latest .
+docker push mphjphk8sacr.azurecr.io/k8s-hub:latest
 cd ..
 
 # build the base-notebook
 cd base-notebook
-docker build -t sandboxmphanaenvacr.azurecr.io/base-notebook:latest .
-docker push sandboxmphanaenvacr.azurecr.io/base-notebook:latest
+docker build -t mphjphk8sacr.azurecr.io/base-notebook:latest .
+docker push mphjphk8sacr.azurecr.io/base-notebook:latest
 cd ..
 
 # drived from base-notebook
 cd r-notebook
-docker build -t sandboxmphanaenvacr.azurecr.io/r-notebook:latest .
-docker push sandboxmphanaenvacr.azurecr.io/r-notebook:latest
+docker build -t mphjphk8sacr.azurecr.io/r-notebook:latest --build-arg BASE_CONTAINER=mphjphk8sacr.azurecr.io/base-notebook:latest .
+docker push mphjphk8sacr.azurecr.io/r-notebook:latest
 cd ..
 
 cd scipy-notebook
-docker build -t sandboxmphanaenvacr.azurecr.io/scipy-notebook:latest .
-docker push sandboxmphanaenvacr.azurecr.io/scipy-notebook:latest
+docker build -t mphjphk8sacr.azurecr.io/scipy-notebook:latest --build-arg BASE_CONTAINER=mphjphk8sacr.azurecr.io/base-notebook:latest .
+docker push mphjphk8sacr.azurecr.io/scipy-notebook:latest
 cd ..
 
 # drived from scipy-notebook & r-notebook
 cd datascience-notebook
-docker build -t sandboxmphanaenvacr.azurecr.io/datascience-notebook:latest .
-docker push sandboxmphanaenvacr.azurecr.io/datascience-notebook:latest
+docker build -t mphjphk8sacr.azurecr.io/datascience-notebook:latest --build-arg BASE_CONTAINER=mphjphk8sacr.azurecr.io/scipy-notebook:latest .
+docker push mphjphk8sacr.azurecr.io/datascience-notebook:latest
 cd ..
 
 cd tensorflow-notebook
-docker build -t sandboxmphanaenvacr.azurecr.io/tensorflow-notebook:latest .
-docker push sandboxmphanaenvacr.azurecr.io/tensorflow-notebook:latest
+docker build -t mphjphk8sacr.azurecr.io/tensorflow-notebook:latest --build-arg BASE_CONTAINER=mphjphk8sacr.azurecr.io/scipy-notebook:latest .
+docker push mphjphk8sacr.azurecr.io/tensorflow-notebook:latest
 cd ..
 
 cd pyspark-notebook
-docker build -t sandboxmphanaenvacr.azurecr.io/pyspark-notebook:latest .
+docker build -t sandboxmphanaenvacr.azurecr.io/pyspark-notebook:latest --build-arg BASE_CONTAINER=mphjphk8sacr.azurecr.io/scipy-notebook:latest .
 docker push sandboxmphanaenvacr.azurecr.io/pyspark-notebook:latest
 cd ..
 
 cd rstudio-notebook
-docker build -t sandboxmphanaenvacr.azurecr.io/rstudio-notebook:latest .
-docker push sandboxmphanaenvacr.azurecr.io/rstudio-notebook:latest
+docker build -t mphjphk8sacr.azurecr.io/rstudio-notebook:latest --build-arg BASE_CONTAINER=mphjphk8sacr.azurecr.io/r-notebook:latest .
+docker push mphjphk8sacr.azurecr.io/rstudio-notebook:latest
 cd ..
 
 cd arcgis-notebook
-docker build -t sandboxmphanaenvacr.azurecr.io/gis-notebook:latest .
-docker push sandboxmphanaenvacr.azurecr.io/gis-notebook:latest
+docker build -t mphjphk8sacr.azurecr.io/gis-notebook:latest --build-arg BASE_CONTAINER=mphjphk8sacr.azurecr.io/scipy-notebook:latest .
+docker push mphjphk8sacr.azurecr.io/gis-notebook:latest
 cd ..
 
 cd sas-notebook
-docker build -t sandboxmphanaenvacr.azurecr.io/sas-notebook:latest .
-docker push sandboxmphanaenvacr.azurecr.io/sas-notebook:latest
+docker build -t mphjphk8sacr.azurecr.io/sas-notebook:latest --build-arg BASE_CONTAINER=mphjphk8sacr.azurecr.io/scipy-notebook:latest .
+docker push mphjphk8sacr.azurecr.io/sas-notebook:latest
 cd ..
 
 # drived from pyspark-notebook
 cd all-spark-notebook
-docker build -t sandboxmphanaenvacr.azurecr.io/all-spark-notebook:latest .
-docker push sandboxmphanaenvacr.azurecr.io/all-spark-notebook:latest
+docker build -t mphjphk8sacr.azurecr.io/all-spark-notebook:latest --build-arg BASE_CONTAINER=mphjphk8sacr.azurecr.io/pyspark-notebook:latest .
+docker push mphjphk8sacr.azurecr.io/all-spark-notebook:latest
 cd ..
 
 
