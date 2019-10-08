5f5ca6f65217b253098391fdb7611aefd3c5a27d
Ziang Jia <zjia@ksmconsulting.com>
2019-10-08 15:42:54 -0400

    add pgadmin4 notebook

---
+++ pgadmin-notebook/jupyter-pgadmin-proxy/jupyter_pgadmin_proxy/__init__.py
@@ -0,0 +1,26 @@
+"""
+Return config on servers to start for theia
+
+See https://jupyter-server-proxy.readthedocs.io/en/latest/server-process.html
+for more information.
+"""
+import os
+import shutil
+
+def setup_pgadmin():
+    # Make sure theia is in $PATH
+    def _pgadmin_command(port):
+        return ['python', '/opt/conda/lib/python3.7/site-packages/pgadmin4/pgAdmin4.py', '--port=' + str(port)]
+
+    return {
+        'command': _pgadmin_command,
+        'environment': {
+            'USE_LOCAL_GIT': 'true'
+        },
+        'timeout': 20,
+        'absolute_url': True,
+        'launcher_entry': {
+            'title': 'PgAdmin IDE',
+            'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons', 'theia.svg')
+        }
+    }
